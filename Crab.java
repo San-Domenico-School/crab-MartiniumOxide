75e1235c44ea63942b78c4cca28e6abb419851fb
MartiniumOxide <ypeng25@sandomenico.org>
2024-09-18 11:33:48 -0700

    1

--- Crab.java
+++ Crab.java
@@ -1,15 +1,52 @@
 import greenfoot.*;
 
 /**
- * This class defines a crab. Crabs live on the beach.
- * @author BGustin
- * @version 6/1/2022 5:48am
- */
+ * This class constructs the player, a crab and behaviors to
+     * provide gameplay including user input, moving and colliding
+     * 
+     * author: Martin, Bryce, Leo, Brittney
+     * version: Sep 16, 2024
+*/
 public class Crab extends Actor
 {
-    // Always place fields first in the class.
-    
+    // 
+      //crab fields
+           private boolean keyPress; //key press true or false
+           
+           private int speed; //magnitude of speed
+         
+           private boolean collision; //is true if collision with fruit
+
+           private boolean isAtCorner; //is true if crab is colliding with corner
+
+           private int yPosition; //position of crab y
+
+           private int xPosition; //position of crab x
+           
     // After the fields place the behaviors
+    
+           /*will change x and y position of crab at magnitude = to speed on key press and stop movement 
+             when keypress = false.
+           */
+           private void crabMovement()
+           {
+               //to be implemented
+           }
+           
+           /*will trigger the turn motion of the crab if atCorner = true.
+           */
+           private void turnAtCorner()
+           {
+               //to be implemented
+           }
+
+           /*will determine if collision is true or false using x/y position relative to fruit.
+           */
+           private void collision()
+           {
+               //to be implemented
+           }
+
 }
 
 
--- CrabWorld.java
+++ CrabWorld.java
@@ -16,7 +16,24 @@ public class CrabWorld extends World
     private void prepareScene()
     {
         // instantiate new objects
-        
+        Crab crab = new Crab();
+    
+    
         // add them to the world
+        addObject(crab, 250,250);
+        // Fruit experiment
+        Fruit fruit = new Fruit( );
+        addObject(fruit, 100, 100);
+        addObject(fruit, 200, 100);
+        Fruit fruit2 = new Fruit( );
+        addObject(fruit2, 300, 100);
+        Fruit fruit3 = new Fruit("Strawberry");
+        Fruit fruit4 = new Fruit("Banana");
+        addObject(fruit3, 100, 200);
+        addObject(fruit4, 200, 200);
+        Fruit fruit5 = new Fruit("Cherry", 30, 1000, 1000);
+        addObject(fruit5, 100, 500);
+
+
     }
 }
